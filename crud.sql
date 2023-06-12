cbc75e0c8aaaaebd9b4894a8d8e2bea17144664d
amitsingh00-coder <72179567+amitsingh00-coder@users.noreply.github.com>
2023-06-12 21:31:43 +0530

    crud.sql
    crud in sql

---
+++ crud.sql
@@ -0,0 +1,20 @@
+--CRUD(Create Read Update Delete)
+FOR  Read
+WHERE Clause in MYSQL
+The WHere Clause is used to filter record
+
+For eg: SELECT *from students Where age=16;
+
+FOR Update
+Use UPDATE statement in MYSQL
+The UPDATE statement is used to modify the exist records in a table
+
+UPDATE students SET age=23
+WHERE name=’ramesh’;
+
+FOR Delete
+Use DELETE statement in MYSQL 
+The Delete statement is used to delete existing record in a table 
+
+DELETE FROM Student WHERE
+    name=’amit’;
