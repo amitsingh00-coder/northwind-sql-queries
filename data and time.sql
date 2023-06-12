9e69d7820b7617ce89e3745292654f937c91b91f
amitsingh00-coder <72179567+amitsingh00-coder@users.noreply.github.com>
2023-06-12 21:44:24 +0530

    date and time.sql
    date and time in sql

---
+++ data and time.sql
@@ -0,0 +1,11 @@
+--MYSQL DATE TIME DATA TYPES
+MYSQL comes with the following data types for storing a date or a date/time value in the database
+
+DATE-format:YYYY-MM-DD
+TIME-format:HH:MI:SS
+DATETIME-format:YYYY-MM-DD
+                                 HH:MI:SS
+
+CURDATE()-GIVES THE CURRENT DATE :YYYY-MM-DD
+NOW()-GIVES BOTH CURDATE & TIME:YYYY-MM-DD:HH:MI:SS
+CURTIME()-Gives Current TIME:HH:MI:SS
