512a0e8c24c1f43b3e391a7abb562561da9b2774
amitsingh00-coder <72179567+amitsingh00-coder@users.noreply.github.com>
2023-06-12 21:39:13 +0530

    aggregrate.sql
    aggregrate in sql

---
+++ aggregrate.sql
@@ -0,0 +1,10 @@
+--MYSQL Aggregate Functions
+MYSQL COUNT(),MAX(),MIN(),AVG() and SUM() Functions
+The count ()function returns the number of rows that matches a specified criteria 
+The AVG() function returns the average values of a numeric column
+The sum() function returns the total sum of a numeric column
+
+Select max(age) from students;
+Select min (age) from students;
+Select sum (age) from students;
+Select count (id) from students ;
