03bba7b523d0daf2bb1c8a24b5dd6cd977c2e998
amitsingh00-coder <72179567+amitsingh00-coder@users.noreply.github.com>
2023-06-12 21:19:52 +0530

    insertdata.sql
    insert data in sql

---
+++ insert data.sql
@@ -0,0 +1,7 @@
+--INSERT DATA INTO TABLES
+INSERT INTO table_name(col1,col2,col3,......)
+VALUES(Value1,Value2,Value3,...);
+              OR
+INSERT INTO table_name
+VALUES(Value1,Value2,Value3,....);
+
