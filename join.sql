9ecebcad7f0ff5cc36b6eb17425a584535b5b736
amitsingh00-coder <72179567+amitsingh00-coder@users.noreply.github.com>
2023-06-12 21:46:53 +0530

    join.sql
    join in sql

---
+++ join.sql
@@ -0,0 +1,11 @@
+  --MYSQL SQL JOIN
+A Join clause is used to combine rows from two or more tables,based on a related column between them
+
+Different types of sql join:
+(INNER) JOIN: Returns records that have matching values in both tables
+
+LEFT(OUTER)JOIN:Returns all records from the left table,and the matched records from the right tables
+
+RIGHT(OUTER)JOIN:Returns all records from the right table,and the matched records from the left table 
+
+
