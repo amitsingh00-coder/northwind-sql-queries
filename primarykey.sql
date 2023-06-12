74040a2533e840a5341de25dac91234e9ff6a4a9
amitsingh00-coder <72179567+amitsingh00-coder@users.noreply.github.com>
2023-06-12 21:28:02 +0530

    primarykey.sql
    primarykey in sql

---
+++ primarykey.sql
@@ -0,0 +1,9 @@
+--what is primary key in Table
+The PRIMARY KEY Constraint uniquely identifies each record in a table primary keys must contain Unique Values,and Cannnot Contain NuLL Values
+
+CREATE TABLE stud_unique
+( stud_id INT NOT NULL
+     ,name VARCHAR(55)
+     ,age INT
+     ,PRIMARY KEY(Stud_id));
+
