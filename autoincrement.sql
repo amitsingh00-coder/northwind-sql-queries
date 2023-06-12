43e91c95f9f7a9c0530535d1f4ea4e61e9b5e71c
amitsingh00-coder <72179567+amitsingh00-coder@users.noreply.github.com>
2023-06-12 21:30:32 +0530

    autoincrement.sql
    autoincrement in sql

---
+++ autoincrement.sql
@@ -0,0 +1,9 @@
+--What is AUTO_INCREMENT
+AUTO_INCREMENT allows a unique number to be generated automatically when a new record is inserted into a table
+
+CREATE TABLE stud_unique
+(Stud_id INT NOT NULL AUTO_INCREMENT
+            ,name VARCHAR(100)
+            ,age   INT
+            , PRIMARY KEY(stud-id));
+
