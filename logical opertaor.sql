2f591f786502f727ba41324736650bf5527441c4
amitsingh00-coder <72179567+amitsingh00-coder@users.noreply.github.com>
2023-06-12 21:40:42 +0530

    logical operator.sql
    logical operator in sql

---
+++ logical opertaor.sql
@@ -0,0 +1,8 @@
+--MYSQL LOGICAL OPERATORS
+ The Logical operator are those that are true or false.They return a true or false values to combine one or more true or more values
+
+Select all students whose age is Not equal to 25;
+Select all students whose age is !=25;
+Select all students where age<25;
+
+Select * from students where age between 22 and 25;
