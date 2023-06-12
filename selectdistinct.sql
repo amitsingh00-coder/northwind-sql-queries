6238f948e4b34b4b106d4b90d6ddb4e2f2be2a4a
amitsingh00-coder <72179567+amitsingh00-coder@users.noreply.github.com>
2023-06-12 21:33:11 +0530

    distinct.sql
    distinct in sql

---
+++ selectdistinct.sql
@@ -0,0 +1,6 @@
+--MYSQL SELECT DISTINCT Statement
+The SELECT DISTINCT Statement is used to Return only distinct (different) values
+SELECT DISTINCT Column1,Column2,
+…..From table_name;
+SELECT DISTINCT fname FROM
+        Students;
